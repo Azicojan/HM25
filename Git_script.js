fc9a1ed69f2328a4e0a335ac254253b1296706c0
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-12 12:01:06 +0500

    adde new features to the secondary branch

--- Git_script.js
+++ Git_script.js
@@ -5,3 +5,4 @@ console.log("I'm learning how to commit in git.");
 console.log('Still learning.)))');
 console.log("Now I'm going to use VS code to commit and push this message.");
 console.log(`This time I'm going to use Git Bash to commit and send this message.`);
+console.log(`This message is being sent to the secondary branch.`);
