c7dc165f04b6327ac0b4e9d64377f81d814f795e
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-12 11:15:57 +0500

    added the 5th change to the file

--- Git_script.js
+++ Git_script.js
@@ -2,3 +2,4 @@ console.log("Hello, world!")
 console.log("Hello, my name is Aziz.");
 console.log("Hy, it's me again.");
 console.log("I'm learning how to commit in git.");
+console.log('Still learning.)))');
