ab60ee65c7e1a1851108263d28f84b87ebdac509
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-12 11:24:13 +0500

    added a new message using Git Bash

--- Git_script.js
+++ Git_script.js
@@ -4,3 +4,4 @@ console.log("Hy, it's me again.");
 console.log("I'm learning how to commit in git.");
 console.log('Still learning.)))');
 console.log("Now I'm going to use VS code to commit and push this message.");
+console.log(`This time I'm going to use Git Bash to commit and send this message.`);
