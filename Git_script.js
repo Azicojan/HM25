5ed0ab5ebe7d488659d0c08d7c68087cdf8acda4
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-12 12:05:28 +0500

    another line to the secondary branch

--- Git_script.js
+++ Git_script.js
@@ -6,3 +6,4 @@ console.log('Still learning.)))');
 console.log("Now I'm going to use VS code to commit and push this message.");
 console.log(`This time I'm going to use Git Bash to commit and send this message.`);
 console.log(`This message is being sent to the secondary branch.`);
+console.lot(`One more time, please!!!`);
