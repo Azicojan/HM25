1e8d65531a6a183f90e3d85913cc96174327c374
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-12 11:20:49 +0500

    used VS code to commit and push

--- Git_script.js
+++ Git_script.js
@@ -3,3 +3,4 @@ console.log("Hello, my name is Aziz.");
 console.log("Hy, it's me again.");
 console.log("I'm learning how to commit in git.");
 console.log('Still learning.)))');
+console.log("Now I'm going to use VS code to commit and push this message.");
