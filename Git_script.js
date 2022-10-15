fb07fd271cf795e59460a279d2317e08831088d6
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-15 11:13:42 +0500

    made a brief conclusion of all the work done so far

--- Git_script.js
+++ Git_script.js
@@ -7,3 +7,8 @@ console.log("Now I'm going to use VS code to commit and push this message.");
 console.log(`This time I'm going to use Git Bash to commit and send this message.`);
 console.log(`Now I'm trying to figure out how to create new branches via Bash.`);
 console.log('message to the bugFix branch.');
+console.log(`Well, I've created a script and HTML files on my computer, added a repo applying git init.Next,
+ I created a remote repo on my Github account and linked it to my local repo. I did these things
+ via Bash and used commands such as git add, git commit and git push. So far so good. It looks like it works,
+ still have a long way to go to completely absorb all of this stuff.Practice makes perfect. By the way, I also
+ created two more branches in addition to the master.`)
