06fe25f31e13a04432b4f8a516a6eebc2b434997
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-15 10:59:39 +0500

    fixed grammatical error

--- Git_script.js
+++ Git_script.js
@@ -1,6 +1,6 @@
 console.log("Hello, world!")
 console.log("Hello, my name is Aziz.");
-console.log("Hy, it's me again.");
+console.log("Hi, it's me again.");
 console.log("I'm learning how to commit in git.");
 console.log('Still learning.)))');
 console.log("Now I'm going to use VS code to commit and push this message.");
